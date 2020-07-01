43314fc8026f58d078f11c93a817c2c91f95f388
Sam <glorbajohanraraon@gmail.com>
2020-07-01 02:54:12 -0400

    if the user logs in correctly, navigate them to the home screen

--- app/src/main/java/com/example/instaclone/LoginActivity.java
+++ app/src/main/java/com/example/instaclone/LoginActivity.java
@@ -2,11 +2,17 @@ package com.example.instaclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -38,5 +44,23 @@ public class LoginActivity extends AppCompatActivity {
     private void loginUser(String username, String password) {
         Log.i(TAG, "Attempting to login user" + username);
         // Navigate to the main activity if the user has signed in properly
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+
+                goMainAcivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void goMainAcivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
